162a5e6a0df1aa77d9956a9f31a17f0b5c5f9549
BLURay <dvoryantsevalexander@gmail.com>
2024-12-16 18:59:56 +0700

    Добавлена функция 9

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ def f3(x, n):
     for i in range(1, n+1):
         answer += ((-1)**i) * (x**(2 * i) / factorial(2 * i))
     return answer
+
+
+def f9(x, m, n):
+    answer = 1
+    for i in range(1, n+1):
+        m_prod = m
+        for j in range(1, i):
+            m_prod *= (m - j)
+        answer += (m_prod * x**i) / factorial(i)
+    return answer
