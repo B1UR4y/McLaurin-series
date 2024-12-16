5d04a7be4706d349e817b4513c0efc2bc038e682
BLURay <dvoryantsevalexander@gmail.com>
2024-12-16 19:20:25 +0700

    Добавлено меню

--- main.py
+++ main.py
@@ -16,3 +16,45 @@ def f9(x, m, n):
             m_prod *= (m - j)
         answer += (m_prod * x**i) / factorial(i)
     return answer
+
+
+def menu(n = 5):
+    while True:
+        try:
+            option = int(input("Выберите одну из опций:\n"
+                         "1 - Функция 3 (cos(x))\n"
+                         "2 - Функция 9 ((1 + x)^m)\n"
+                         # "3 - Функция 10\n"
+                         "4 - Выход\n"))
+        except:
+            print("Допустим только ввод целых чисел")
+            continue
+        match option:
+            case 1:
+                try:
+                    x = float(input("Введите X: "))
+                except:
+                    print("Допустим только ввод чисел")
+                    continue
+                print(f3(x, n))
+            case 2:
+                try:
+                    x = float(input("Введите X: "))
+                    m = float(input("Введите M: "))
+                except:
+                    print("Допустим только ввод чисел")
+                    continue
+                if not -1 < x < 1:
+                    print("X должен быть больше -1 и меньше 1")
+                    continue
+                print(f9(x, m, n))
+            case 3:
+                pass
+            case 4:
+                return 0
+            case _:
+                print("Допустим только ввод чисел от 1 до 3 включительно")
+                # После добавление функции 10 - от 1 до 4
+
+
+menu()
